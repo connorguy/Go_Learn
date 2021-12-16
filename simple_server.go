4c45324d7be45294d8d4525544ef761a17505475
connorguy <connor_guy@icloud.com>
2021-12-16 09:10:44 -0800

    wip

--- simple_server.go
+++ simple_server.go
@@ -5,10 +5,35 @@
 
 import(
     "net/http"
+	"fmt"
+    "os"
 )
 
+const (
+	set = "set"
+	getKey = "get"
+)
+
+inMemoryKeyValueStore := make(map[string]string)
+
+func logAnErrorAndExit(err string){
+	log.Error("Error: %s", err)
+	os.Exit(-1)
+}
+
 func set(w http.ResponseWriter, req *http.Request) {
-	
+	if err = req.ParseForm(); err != nil {
+		logAnErrorAndExit("Error parsing set")
+	}
+	reqKeyValueMap := req.Form.Get(set)
+	if len(reqKeyValueMap) !=1{
+		logAnErrorAndExit("invalid request for set.")
+	}
+	key := 1
+	value :=1
+
+	// Store the value now
+	inMemoryKeyValueStore[key] = value
 }
 
 func get()(w http.ResponseWriter, req *http.Request) {
